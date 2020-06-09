0edc2e4614422facf4fa7d295d841a68be4d6f8a
StephanyValarezo <valarezo@fiec.espol.edu.ec>
2020-06-08 19:28:28 -0500

    se agrego clase GenricAPIView

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     #path('article/', article_list),
     path('article/', ArticleAPIView.as_view()), #porque es un clase agregas el .as_view()
     #path('detail/<int:pk>/', article_detail),
-     path('detail/<int:id>/', ArticleDetails.as_view()),
-   
+    path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('genericview/<int:id>/', GenericAPIView.as_view()),
+
 ]
